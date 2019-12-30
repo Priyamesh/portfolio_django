12f4ce29ce45d4d09682a72f50a82dcae0db6a2a
Priyamesh <priyamesh28@gmail.com>
2019-12-30 23:38:45 +0530

    final backend

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,14 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.tittle
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_display(self):
+        return self.pub_date.strftime('%b %d %Y')
     
 
